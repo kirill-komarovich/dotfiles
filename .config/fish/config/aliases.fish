cbfceecc82128abae5cd4e74a914fe6a24f617c1
Kirill Komarovich <still2405@gmail.com>
2021-10-18 21:24:06 +0300

    Fix user env var in fish aliases

--- .config/fish/config/aliases.fish
+++ .config/fish/config/aliases.fish
@@ -5,7 +5,7 @@ alias dcr='docker-compose run --rm'
 alias dce='docker-compose exec'
 alias dcrs='docker-compose restart'
 alias dcl='docker-compose logs -f'
-alias dcchown='sudo chown -R USER'
+alias dcchown='sudo chown -R $USER'
 
 # alias livebook-up='docker run -d --rm --name livebook -p 127.0.0.1:8080:8080 -u (id -u):(id -g) -v ~/projects/elixir/.livebook:/data livebook/livebook'
 alias livebook-up='docker run -d --rm --name livebook -p 8080:8080 -e LIVEBOOK_PASSWORD=f6fbd3f19ec020b64a62967c84c1c8e4 -u (id -u):(id -g) -v ~/projects/elixir/.livebook:/data livebook/livebook'
