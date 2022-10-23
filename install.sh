3bfcb490b69d7e83e10bff29cf46a70fdcba41b7
Kirill Komarovich <still2405@gmail.com>
2022-10-23 20:24:39 +0300

    Add install packages script

--- install.sh
+++ install.sh
@@ -4,18 +4,20 @@ set -e # -e: exit on error
 
 if [ ! "$(command -v chezmoi)" ]; then
   bin_dir="$HOME/.local/bin"
+  export PATH=$PATH:bin_dir
+
   chezmoi="$bin_dir/chezmoi"
 
   if [ "$(command -v curl)" ]; then
     sh -c "$(curl -fsSL https://git.io/chezmoi)" -- -b "$bin_dir"
   else
-    echo "To install chezmoi, you must have curl or wget installed." >&2
+    echo "To install chezmoi, you must have curl installed." >&2
     exit 1
   fi
 else
   chezmoi=chezmoi
 fi
 
-dotfiles_dir="$(CDPATH= cd -- "$(dirname -- "$0")" && pwd)"
+script_dir="$(cd -P -- "$(dirname -- "$(command -v -- "$0")")" && pwd -P)"
 
-exec "$chezmoi" init --apply "--source=$dotfiles_dir"
+exec "$chezmoi" init --apply "--source=$script_dir"
