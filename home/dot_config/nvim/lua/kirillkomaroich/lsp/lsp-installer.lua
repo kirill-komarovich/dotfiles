5e648a7e8e9f7e8e48b5ce00dfe8d636eef786e4
Kirill Komarovich <still2405@gmail.com>
2023-02-09 13:16:06 +0100

    Refactor lsp auto setup

--- home/dot_config/nvim/lua/kirillkomaroich/lsp/lsp-installer.lua
+++ home/dot_config/nvim/lua/kirillkomaroich/lsp/lsp-installer.lua
@@ -1,5 +1,4 @@
 local mason_config = require("mason-lspconfig")
-local lspconfig = require("lspconfig")
 local handlers = require("kirillkomaroich.lsp.handlers")
 
 require("mason").setup()
@@ -22,17 +21,19 @@ local opts = {
   capabilities = handlers.capabilities,
 }
 
-for _, server in ipairs(mason_config.get_installed_servers()) do
-  local local_opts = {}
+mason_config.setup_handlers({
+  function (server_name)
+    local local_opts = {}
 
-  local settings_path = string.format("kirillkomaroich.lsp.settings.%s", server)
-  local status_ok, server_opts = pcall(require, settings_path)
+    local settings_path = string.format("kirillkomaroich.lsp.settings.%s", server_name)
+    local status_ok, server_opts = pcall(require, settings_path)
 
-  if status_ok then
-    local_opts = vim.tbl_deep_extend("force", server_opts, opts)
-  else
-    local_opts = vim.tbl_deep_extend("force", local_opts, opts)
-  end
+    if status_ok then
+      local_opts = vim.tbl_deep_extend("force", server_opts, opts)
+    else
+      local_opts = vim.tbl_deep_extend("force", local_opts, opts)
+    end
 
-  lspconfig[server].setup(local_opts)
-end
+    require("lspconfig")[server_name].setup(local_opts)
+  end,
+})
