48323e4fce92bcc709e7d4c82f6e9e14d3e9ccc1
Kirill Komarovich <still2405@gmail.com>
2023-11-08 11:52:02 +0100

    Update indent_blanckline config

--- home/dot_config/nvim/after/plugin/indent_blankline.lua
+++ home/dot_config/nvim/after/plugin/indent_blankline.lua
@@ -2,8 +2,7 @@ vim.opt.list = true
 vim.opt.listchars:append "space:⋅"
 vim.opt.listchars:append "eol:↴"
 
-require("indent_blankline").setup {
-  show_end_of_line = true,
-  space_char_blankline = " ",
-  char = "▏",
+require("ibl").setup {
+  indent = { char = "▏" },
+  scope = { enabled = false },
 }
