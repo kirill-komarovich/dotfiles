b61c4acd39bdf7c61cf32e0eeafd4cbab28d87a4
Kirill Komarovich <still2405@gmail.com>
2023-11-28 12:54:18 +0100

    Fix Comment config

--- home/dot_config/nvim/lua/kirillkomarovich/plugins.lua
+++ home/dot_config/nvim/lua/kirillkomarovich/plugins.lua
@@ -65,15 +65,14 @@ require("lazy").setup({
 
   {
     "numToStr/Comment.nvim",
-    event = "BufEnter",
     dependencies = {
       "JoosepAlviste/nvim-ts-context-commentstring",
     },
-    config = function(comment)
+    config = function()
       require('ts_context_commentstring').setup({})
       vim.g.skip_ts_context_commentstring_module = true
 
-      comment.setup({
+      require("Comment").setup({
         pre_hook = require('ts_context_commentstring.integrations.comment_nvim').create_pre_hook(),
       })
     end,
