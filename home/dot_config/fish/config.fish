c48a668371f7420018285453cb48b311ac005faf
Kirill Komarovich <still2405@gmail.com>
2024-02-18 14:52:25 +0100

    Add asdf java setup for fish

--- home/dot_config/fish/config.fish
+++ home/dot_config/fish/config.fish
@@ -4,6 +4,10 @@ if test $HOME/.asdf/asdf.fish
   if not test -d ~/.config/fish/completions
     mkdir -p ~/.config/fish/completions; and ln -s ~/.asdf/completions/asdf.fish ~/.config/fish/completions
   end
+
+  if test -d $HOME/.asdf/plugins/java
+    source $HOME/.asdf/plugins/java/set-java-home.fish
+  end
 end
 
 # function fish_greeting
