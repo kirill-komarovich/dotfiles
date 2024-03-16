99cef31427a43c4b2089914292b4d71085713a8b
Kirill Komarovich <still2405@gmail.com>
2024-03-16 22:27:11 +0100

    Mark jbuilder nad Dangerfile as ruby files

--- home/dot_config/nvim/after/exact_plugin/autocommands.lua
+++ home/dot_config/nvim/after/exact_plugin/autocommands.lua
@@ -2,7 +2,8 @@ local nvim_create_augroup = vim.api.nvim_create_augroup
 local nvim_create_autocmd = vim.api.nvim_create_autocmd
 local nvim_command = vim.api.nvim_command
 
-GeneralGroup = nvim_create_augroup('General', { clear = true })
+GeneralGroup = nvim_create_augroup("General", { clear = true })
+RubyGroup = nvim_create_augroup("Ruby", { clear = true })
 
 -- Trim Whitespaces at the end of line
 nvim_create_autocmd("BufWritePre", {
@@ -20,6 +21,22 @@ nvim_create_autocmd("BufWritePre", {
   callback = function()
     nvim_command("%s/\\($\\n\\s*\\)\\+\\%$//e")
   end,
+})
+
+nvim_create_autocmd("FileType", {
+  pattern = "ruby",
+  group = RubyGroup,
+  callback = function()
+    vim.cmd.setlocal("indentkeys-=.")
+  end
+})
+
+nvim_create_autocmd("BufRead", {
+  pattern = "*.jbuilder,Dangerfile",
+  group = RubyGroup,
+  callback = function()
+    vim.cmd.set("ft=ruby")
+  end
 })
 
 PackagesGroup = nvim_create_augroup('Packages', { clear = true })
