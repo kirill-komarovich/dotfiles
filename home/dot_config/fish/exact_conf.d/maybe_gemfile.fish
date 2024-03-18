c960cf5c06d815d57fc5a2f6f1e20d6d34af0c51
Kirill Komarovich <still2405@gmail.com>
2024-03-18 16:15:19 +0100

    Speedup maybe_gemfile

--- home/dot_config/fish/exact_conf.d/maybe_gemfile.fish
+++ home/dot_config/fish/exact_conf.d/maybe_gemfile.fish
@@ -1,7 +1,7 @@
 function define_bundle_context_functions --on-variable MAYBE_GEMFILE_COMMANDS
   for name in (string split : $MAYBE_GEMFILE_COMMANDS)
     function $name --wraps $name
-      if test -e Gemfile -a (gem list "^($_)\$" -i) = true
+      if test -e Gemfile; and grep -q "$name" Gemfile
         echo "Using Gemfile $_"
         command bundle exec $_ $argv
       else
