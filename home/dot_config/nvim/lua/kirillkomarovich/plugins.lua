2e3ab518189135968bc1e63622d5d637e6284f2e
Kirill Komarovich <still2405@gmail.com>
2024-03-24 11:20:12 +0100

    Remove null-ls

--- home/dot_config/nvim/lua/kirillkomarovich/plugins.lua
+++ home/dot_config/nvim/lua/kirillkomarovich/plugins.lua
@@ -129,8 +129,6 @@ require("lazy").setup({
     dependencies = {
       "williamboman/mason.nvim",
       "williamboman/mason-lspconfig.nvim",
-      "jose-elias-alvarez/null-ls.nvim",
-      "jay-babu/mason-null-ls.nvim",
     },
   },
 
