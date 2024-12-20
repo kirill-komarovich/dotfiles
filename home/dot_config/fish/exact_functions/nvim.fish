0f86cea79de0abd6225043556dfdf3ff505c2519
Kirill Komarovich <still2405@gmail.com>
2024-12-20 18:38:49 +0100

    Drop fish nvim wrapper

--- home/dot_config/fish/exact_functions/nvim.fish
+++
@@ -1,9 +0,0 @@
-function nvim --wraps nvim
-  if test -e .git/config
-    set pipe_name (basename (pwd))
-
-    command nvim --listen $HOME/.cache/nvim/$pipe_name.pipe $argv
-  else
-    command nvim $argv
-  end
-end
