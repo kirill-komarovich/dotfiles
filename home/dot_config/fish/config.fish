d772650b10be095bfc3d08f899362f44862d5ab9
Kirill Komarovich <still2405@gmail.com>
2025-05-05 17:49:34 +0200

    Add pnpm home for pnpm link

--- home/dot_config/fish/config.fish
+++ home/dot_config/fish/config.fish
@@ -19,3 +19,8 @@ end
 # Added by OrbStack: command-line tools and integration
 # This won't be added again if you remove it.
 source ~/.orbstack/shell/init2.fish 2>/dev/null || :
+
+set -gx PNPM_HOME "$HOME/Library/pnpm"
+if not string match -q -- $PNPM_HOME $PATH
+  set -gx PATH "$PNPM_HOME" $PATH
+end
