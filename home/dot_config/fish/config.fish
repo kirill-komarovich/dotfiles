c0bd25abf5c0dde9bec96d2d4877b9a6eb679113
Kirill Komarovich <still2405@gmail.com>
2025-05-05 17:49:13 +0200

    Add orbstack source

--- home/dot_config/fish/config.fish
+++ home/dot_config/fish/config.fish
@@ -15,3 +15,7 @@ if test $HOME/.local/bin/mise
     $mise activate fish --shims | source
   end
 end
+
+# Added by OrbStack: command-line tools and integration
+# This won't be added again if you remove it.
+source ~/.orbstack/shell/init2.fish 2>/dev/null || :
