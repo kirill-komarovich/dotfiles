6352c5ab79d072b6a6ac536aa7216c73c1bc4985
Kirill Komarovich <still2405@gmail.com>
2026-09-06 13:58:05 +0200

    Fix fish path rebuild

--- home/dot_config/fish/exact_conf.d/vars.fish
+++ home/dot_config/fish/exact_conf.d/vars.fish
@@ -2,4 +2,4 @@ set -gx EDITOR nvim-open
 set -gx VISUAL nvim-open
 set -gx ERL_AFLAGS "-kernel shell_history enabled"
 set -gx RIPGREP_CONFIG_PATH $HOME/.config/ripgrep/config
-fish_add_path -gP "$HOME/.local/bin" "$HOME/.cargo/bin"
+fish_add_path -gPm "$HOME/.local/bin" "$HOME/.cargo/bin"
